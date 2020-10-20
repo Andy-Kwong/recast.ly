3df3cdf856706302b5ef7e88c6ca776bf11b2769
Andrew Kwong <kwong.andy93@gmail.com>
2020-10-20 06:34:11 -0700

    Adds imports and refactors to class

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,65 @@
-var App = () => (
-  <div>
-    <nav className="navbar">
-      <div className="col-md-6 offset-md-3">
-        <div><h5><em>search</em> view goes here</h5></div>
+
+import VideoList from './VideoList.js';
+import VideoPlayer from './VideoPlayer.js';
+import exampleVideoData from '../data/exampleVideoData.js';
+// var App = () => (
+//   <div>
+//     <nav className="navbar">
+//       <div className="col-md-6 offset-md-3">
+//         <div><h5><em>search</em> view goes here</h5></div>
+//       </div>
+//     </nav>
+//     <div className="row">
+//       <div className="col-md-7">
+//         <div><h5><em>videoPlayer</em> view goes here</h5></div>
+//       </div>
+//       <div className="col-md-5">
+//         {/* <VideoList video={exampleVideoData} /> */}
+//         <div>
+//           <em>videoList</em>
+//           <VideoList video={exampleVideoData} />
+//         </div>
+//       </div>
+//     </div>
+//   </div>
+// );
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <nav className="navbar">
+          <div className="col-md-6 offset-md-3">
+            <div><h5><em>search</em> view goes here</h5></div>
+          </div>
+        </nav>
+        <div className="row">
+          <div className="col-md-7">
+            <div>
+              <em>videoPlayer</em>
+              <VideoPlayer video={exampleVideoData[0]} />
+            </div>
+          </div>
+          <div className="col-md-5">
+            <div>
+              <em>videoList</em>
+              <VideoList videos={exampleVideoData} func={this.handleClick} />
+            </div>
+          </div>
+        </div>
       </div>
-    </nav>
-    <div className="row">
-      <div className="col-md-7">
-        <div><h5><em>videoPlayer</em> view goes here</h5></div>
-      </div>
-      <div className="col-md-5">
-        <div><h5><em>videoList</em> view goes here</h5></div>
-      </div>
-    </div>
-  </div>
-);
+    );
+  }
+
+  // Method handleClick()
+  handleClick() {
+    console.log('HELLO');
+  }
+}
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
